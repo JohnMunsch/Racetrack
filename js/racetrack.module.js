a54889ad159dbd5e6a9a50ecae7c09a3576b5dfe
John Munsch <john.munsch@gmail.com>
2015-05-28 16:32:17 -0500

    Trying to fix some path errors when the app is run on Github pages.

--- js/racetrack.module.js
+++ js/racetrack.module.js
@@ -2,10 +2,10 @@ angular.module('Racetrack', ['ngRoute'])
     .config(function ($routeProvider, $locationProvider) {
       $routeProvider
           .when('/', {
-            templateUrl: '/views/main.html'
+            templateUrl: 'views/main.html'
           })
           .when('/about', {
-            templateUrl: '/views/about.html'
+            templateUrl: 'views/about.html'
           })
           .otherwise('/');
     });
